35860835dda5d05676c609df373471f94fcd8a3e
Rukhsar Bano <afsanabegum6430@gmail.com>
2026-02-15 00:11:29 +0530

    Fix lottery game logic

--- lottery-game/src/App.jsx
+++ lottery-game/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Lottery from './Lottery'
+import {sum} from './helper'
 
 function App() {
 
+  let winCondition = (ticket) => {
+    return ticket.every((num) => num === ticket[0]);
+  }
+
   return (
     <>
-    <Lottery n={3} winningSum={15}/>
+    <Lottery n={3} winCondition={winCondition}/>
     </>
   )
 }
