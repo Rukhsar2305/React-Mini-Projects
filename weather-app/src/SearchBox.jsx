7b9cadcc85fd9313773e1ad01510766d94c3a8c4
Rukhsar Bano <afsanabegum6430@gmail.com>
2026-02-22 23:41:37 +0530

    Update SearchBox.jsx

--- weather-app/src/SearchBox.jsx
+++ weather-app/src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({updateInfo}) {
     let [city, setCity] = useState("");
 
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY = "bc0728175bc6d539d8b9b3a05154da82";
+    const API_KEY = "";
 
     let getWeatherInfo = async() => {
         let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
