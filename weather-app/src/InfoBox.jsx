2448e9d973f2dac8f7d8526bc16345f18943eb8e
Rukhsar Bano <afsanabegum6430@gmail.com>
2026-02-22 22:13:09 +0530

    update

--- weather-app/src/InfoBox.jsx
+++ weather-app/src/InfoBox.jsx
@@ -3,21 +3,34 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import "./InfoBox.css";
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import SunnyIcon from '@mui/icons-material/Sunny';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 
 export default function InfoBox({info}) {
-    
+    const INIT_URL = "https://images.unsplash.com/photo-1628534262701-0ada95e46152?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const HOT_URL = "https://images.unsplash.com/photo-1504370805625-d32c54b16100?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const COLD_URL = "https://images.unsplash.com/photo-1668531387310-9c3c2f272d52?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const RAIN_URL = "https://img.freepik.com/free-photo/photorealistic-view-tree-nature-with-branches-trunk_23-2151478106.jpg?semt=ais_hybrid&w=740&q=80";
+
     return(
         <div className='InfoBox'>
         <div className='cardContainer'>
         <Card sx={{ maxWidth: 345 }}>
             <CardMedia
             sx={{ height: 140 }}
-            image="https://images.unsplash.com/photo-1628534262701-0ada95e46152?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            image={info.humidity > 80 ? RAIN_URL : info.temp > 15 ? HOT_URL : COLD_URL}
             title="green iguana"
         />
         <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-                {info.city}
+                {info.city} {
+                info.humidity > 80 
+                ? <ThunderstormIcon/> 
+                : info.temp > 15 
+                ? <SunnyIcon/>
+                : <AcUnitIcon/>
+            }
             </Typography>
             <Typography variant="body2" sx={{ color: 'text.secondary' }} component="span">
                 <p>Humidity = {info.humidity}</p>
